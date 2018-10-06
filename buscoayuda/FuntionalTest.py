2df9a4d6200184157ebd2f9fe01e3a4418c94568
mlopeza1 <mlopeza1@ucentral.edu.co>
2018-10-06 00:57:06 -0500

    Paso 1 Busqueda de titulo encontrado (Verde)

--- buscoayuda/FuntionalTest.py
+++ buscoayuda/FuntionalTest.py
@@ -2,10 +2,16 @@ import os
 from unittest import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import sys
 
 class FunctionalTest(TestCase):
     def setUp(self):
-        self.browser = webdriver.Chrome('C://chromedriver.exe')
+        sistemaop = sys.platform
+        print('so ' + sistemaop)
+        if sistemaop == 'linux':
+            self.browser = webdriver.Chrome()
+        elif sistemaop == 'win32':
+            self.browser = webdriver.Chrome('C://chromedriver.exe')
 
     def tearDown(self):
         self.browser.quit()
