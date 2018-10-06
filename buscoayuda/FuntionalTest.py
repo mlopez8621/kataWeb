c07b5f9552bac408203a09b4ed0bda06825eb89f
osabogal10 <o.sabogal10@uniandes.edu.co>
2018-10-06 00:11:05 -0500

    Paso 1 Busqueda de titulo no encontrado (Rojo)

--- buscoayuda/FuntionalTest.py
+++ buscoayuda/FuntionalTest.py
@@ -5,11 +5,12 @@ from selenium.webdriver.common.by import By
 
 class FunctionalTest(TestCase):
     def setUp(self):
-        self.browser = webdriver.Chrome('C//chromedriver.exe')
+        self.browser = webdriver.Chrome('C://chromedriver.exe')
 
     def tearDown(self):
         self.browser.quit()
 
     def test_1_title(self):
-        self.browser.get('http://www.google.com.co')
-        self.assertIn('Google', self.browser.title)
+        self.browser.get('http://localhost:8000')
+        print(self.browser.title)
+        self.assertIn('Busco ayuda', self.browser.title)
