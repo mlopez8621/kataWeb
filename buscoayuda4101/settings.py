63a89b88ad278bff38a28e3d07d6a9aa3b4dc098
osabogal10 <o.sabogal10@uniandes.edu.co>
2018-10-08 22:48:02 -0500

    corriendo

--- buscoayuda4101/settings.py
+++ buscoayuda4101/settings.py
@@ -122,7 +122,7 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 #STATICFILES_DIRS = [
